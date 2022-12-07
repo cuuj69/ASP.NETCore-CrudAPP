1b13d13ad7a88f7d90bc5887172469260a6e5891
cuuj69 <mensahjefferson69@gmail.com>
2022-12-07 18:33:34 +0000

    Added files

---
+++ WorkersAPI/Controllers/WorkersController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkersAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class WorkersController : ControllerBase {
+
+        private static List<Workers> board = new List<Workers>
+            {
+                new Workers {
+                     Id = 1,
+                     Position = "Manager",
+                     FirstName = "John",
+                     LastName = "Afolabi",
+                     Location = "Accra"
+
+                },
+
+           new Workers {
+                    Id = 2,
+                    Position = "Branch Manager",
+                    FirstName = "Kweku",
+                    LastName = "Adu",
+                    Location = "Kumasi"
+
+                }
+           };
+
+
+        [HttpGet]
+
+        public async Task<ActionResult<List<Workers>>> Get()
+        {
+
+            return Ok(board);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Workers>> Get(int id)
+        {
+            var member = board.Find(h => h.Id == id);
+            if (member == null)
+                return BadRequest("Id does not match any worker");
+            return Ok(board);
+        }
+
+
+
+        [HttpPost]
+
+        public async Task<ActionResult<List<Workers>>> AddWorker(Workers member)
+        {
+            board.Add(member);
+            return Ok(board);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Workers>>> UpdateWorker(Workers request)
+        {
+            var member = board.Find(h => h.Id == request.Id);
+            if (member == null)
+                return BadRequest("Id does not match any worker");
+
+            member.Position = request.Position;
+            member.FirstName = request.FirstName;
+            member.LastName = request.LastName;
+            member.Location = request.Location;
+
+            return Ok(board);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Workers>>> Delete(int id)
+        {
+            var member = board.Find(h => h.Id == id);
+            if (member == null)
+                return BadRequest("Id does not match any worker");
+
+            board.Remove(member);
+            return Ok(board);
+        }
+
+    }
+}
+
---
+++ WorkersAPI/Workers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkersAPI
+{
+    public class Workers
+    {
+        public int Id { get; set; }
+
+        public string Position { get; set; } = string.Empty;
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Location { get; set; } = string.Empty;
+
+       
+    }
+}
